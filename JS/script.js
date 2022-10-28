e790d6f33f64f020227d47dc0ad29a616465f988
Samtoro <105719435+Pequod96@users.noreply.github.com>
2022-10-28 16:01:35 +0200

    updates

--- JS/script.js
+++ JS/script.js
@@ -1,14 +1,23 @@
 
-search.onclick = function(){
-   let book = document.getElementById('book').value;
-    if(book != 'fantasy' && book != 'Alice in wonderland')
-    return (alert('Errore generico'));
+ const api_url = 'https://openlibrary.org/subjects/fantasy.json';
+async function getFantasy(){
+  const response = await fetch(api_url);
+  const data = await response.json();
+  let arrayy = data.works;
+  //console.log(data.works[0].authors[0].name);
+  for(let i=0; i<arrayy.length; i++){
+    console.log(arrayy[i]);
+   let space = "<br></br>";
+    document.getElementById('res').innerHTML += "<a href = 'https://openlibrary.org/" + arrayy[i].key + "'> " + arrayy[i].title  + space +" </a>";
+  } 
 }
 
- fetch('https://openlibrary.org/subjects/fantasy.json')
- .then(response => {
-   return response.json();
- }).then(json => {
-    console.log('fetch', json);
- 
- })
+  const button = document.querySelector('button');
+  button.onclick = function(){
+    let book = document.getElementById('book').value;
+    if(book == 'fantasy'){
+    return getFantasy();
+    } else {(book != 'fantasy')
+    return alert('Dite "fantasy" ed entrate!');
+    }
+  }
